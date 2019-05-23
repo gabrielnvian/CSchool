3d7b51cef9fbdec1a257a2fc2f54fbbe1884b33a
Gabriel Vian <g@gabrielvian.com>
2019-05-23 19:15:32 +0200

    added comments

--- 3LSA/Lezione 1/main.cpp
+++ 3LSA/Lezione 1/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
+// Prende 10 numeri dall'utente e ritorna il maggiore
 void maggiore() {
-    // Prende 10 numeri dall'utente e ritorna il maggiore
     int a;
     int max;
 
+    // Prende in numeri
     for (int i = 0; i < 10; i++) {
         scanf("%d", &a);
+        // Se il numero è maggiore di quello dopo || è il primo => scambiali
         if (a > max || i == 0) {
             max = a;
         }
@@ -14,8 +16,8 @@ void maggiore() {
     printf("%d", max);
 }
 
+// Prende n numeri dall'utente e li ritorna in ordine crescente
 void crescente() {
-    // Prende n numeri dall'utente e li ritorna in ordine crescente
     int n, a;
     int i = 0;
     printf("Quanti numeri? ");
@@ -23,18 +25,17 @@ void crescente() {
 
     int num1[n];
 
+    // Prende i numeri
     for (i = 0; i < n; i++) {
         scanf("%d", &num1[i]);
     }
 
-    i = 0;
-    while(i < n) {
+    for (i = 0; i < n; i++) {
         if (num1[i] >= num1[i+1]) {
             a = num1[i];
             num1[i] = num1[i+1];
             num1[i+1] = a;
         }
-        i++;
     }
 
     for (i = 0; i < n; i++) {
@@ -42,6 +43,7 @@ void crescente() {
     }
 }
 
+// Prende n numeri dall'utente e ritorna solo i multipli di 3
 void multipli() {
     int n, a;
     int i = 0;
@@ -57,9 +59,10 @@ void multipli() {
         i++;
     }
 
+    // Ritorna i numeri solo se num % 3 == 0
     for (i = 0; i < n; i++) {
-        if (a % 3 == 0) {
-            printf("%d", num1[i]);
+        if (num1[i] % 3 == 0) {
+            printf("%d\n", num1[i]);
         }
     }
 }
